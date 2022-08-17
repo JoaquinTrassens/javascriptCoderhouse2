3355407f9d3f7b80d217f1be44b11b5a638d4095
Joaquin <trassensjoaquin@gmail.com>
2022-08-17 09:40:42 -0300

    agregue el ejercicio de bucles al repositorio

---
+++ desafioClase3/main.js
@@ -0,0 +1,15 @@
+// Desafio complementario de la clase 3
+
+/*
+ejercicio creado por mi para el desafio.
+
+El ejercicio pide ingresar un numero al usuario y repetir un mensaje la cantidad de veces que haya indicado
+*/
+
+let numeroAIngresar = parseInt(prompt(`Ingrese un numero: `));
+
+for(i=1; i<=numeroAIngresar; i++) {
+    console.log(`este mensaje se repetira la cantidad de veces segun el numero ingresado`);
+    alert(`este mensaje se repetira la cantidad de veces segun el numero ingresado`);
+}
+
