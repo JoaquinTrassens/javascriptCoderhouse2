023e01fdec6559333c0d69a5a08508da5c9d8145
Joaquin <trassensjoaquin@gmail.com>
2022-09-18 21:16:03 -0300

    agregando cosas a la segundaEntrega

---
+++ segundaEntregaProyectoFinal/JS/main.js
@@ -0,0 +1,40 @@
+// VARIABLES
+const productosCaninos = [
+    {
+        id:1,
+        nombre: `Shampoo pelaje blanco 300ml`,
+        img: './multimedia/shampoo1'
+    },
+    {
+        id:2,
+        nombre: `Shampoo jabon blanco cremoso 300ml`,
+        img: './multimedia/shampoo2'
+    },
+    {
+        id:3,
+        nombre: `Sampoo 2 en 1 300ml`,
+        img: './multimedia/shampoo3'
+    },
+    {
+        id:4,
+        nombre: `Shampoo neutro belleza 300ml`,
+        img: './multimedia/shampoo4'
+    },
+    {
+        id:5,
+        nombre: `Shampoo sensible canela y manzana 300ml`,
+        img: './multimedia/shampoo5'
+    },
+    {
+        id:6,
+        nombre: `Shampoo puppy 300ml`,
+        img: './multimedia/shampoo6'
+    }
+]
+
+const contenedorProductos = document.querySelector(".contenedorProductos");
+
+// EVENTOS
+
+
+// FUNCIONES
